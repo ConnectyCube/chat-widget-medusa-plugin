07292216fcadab08f835184ce20f8861d29e8df6
CCAdmin <admin@connectycube.com>
2025-05-07 15:36:36 +0300

    pass envs via __VITE_CHAT_APP_ID__, __VITE_CHAT_AUTH_KEY__

--- src/admin/routes/chat/page.tsx
+++ src/admin/routes/chat/page.tsx
@@ -1,15 +1,18 @@
-import { defineRouteConfig } from "@medusajs/admin-sdk"
-import { ChatBubble, Spinner } from "@medusajs/icons"
-import ConnectyCubeChatWidget from '@connectycube/chat-widget';
+import { defineRouteConfig } from "@medusajs/admin-sdk";
+import { ChatBubble, Spinner } from "@medusajs/icons";
+import ConnectyCubeChatWidget from "@connectycube/chat-widget";
 import { Container } from "@medusajs/ui";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { useStore } from "../../hooks/api/store";
 
 const Chat = () => {
-  const navigate = useNavigate()
-  const { store, isLoading } = useStore({fields: 'id,name'}, { refetchOnMount: "always" })
-  
+  const navigate = useNavigate();
+  const { store, isLoading } = useStore(
+    { fields: "id,name" },
+    { refetchOnMount: "always" }
+  );
+
   useEffect(() => {
     if (!store && !isLoading) {
       navigate("/404");
@@ -21,41 +24,49 @@ const Chat = () => {
       <div className="flex min-h-screen items-center justify-center">
         <Spinner className="text-ui-fg-interactive animate-spin" />
       </div>
-    )
+    );
   }
 
   const embedViewStyles: React.CSSProperties = {
-    position: 'relative',
-    display: 'flex',
-    right: '0',
-    top: '0',
-    minWidth: '100%',
-    minHeight: '100%',
-    zIndex: '0',
+    position: "relative",
+    display: "flex",
+    right: "0",
+    top: "0",
+    minWidth: "100%",
+    minHeight: "100%",
+    zIndex: "0",
   };
+
+  //@ts-ignore
+  const appId = import.meta.env.VITE_CHAT_APP_ID || __VITE_CHAT_APP_ID__;
+  //@ts-ignore
+  const authKey = import.meta.env.VITE_CHAT_AUTH_KEY || __VITE_CHAT_AUTH_KEY__;
 
   return (
     <Container>
-      <ConnectyCubeChatWidget   
-        appId={import.meta.env.VITE_CHAT_APP_ID}
-        authKey={import.meta.env.VITE_CHAT_AUTH_KEY}
+      <ConnectyCubeChatWidget
+        appId={appId}
+        authKey={authKey}
         userId={store.id}
         userName={store.name}
         splitView={true}
         open={true}
         hideWidgetButton={true}
         portalStyle={embedViewStyles}
-        onUnreadCountChange={(count: number) => console.log('unread messages count:', count)}
-        onOnlineUsersCountChange={(count: number) => console.log('online users count:', count)}
+        onUnreadCountChange={(count: number) =>
+          console.log("unread messages count:", count)
+        }
+        onOnlineUsersCountChange={(count: number) =>
+          console.log("online users count:", count)
+        }
       />
     </Container>
-  )
-}
-
+  );
+};
 
 export const config = defineRouteConfig({
   label: "Chat",
   icon: ChatBubble,
-})
+});
 
-export default Chat
+export default Chat;
